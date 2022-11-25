22cff22cbb5a0b7f4b3f2842e5003f83acae0471
= <=>
2022-11-25 22:19:52 +0530

    UI initial Draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Factory  from "./Contracts/Factory.json";
+import Wallet from "./Contracts/Wallet.json";
 
-function App() {
+import { ethers } from "ethers";
+import {config} from "./config"
+
+export default function App() {
+  const [factoryContract, setFactoryContract] = useState();
+  const [walletContract, setWalletContract] =  useState();
+
+  const connectWallet = async () => {
+
+    try {
+      if (!window.ethereum) {
+        alert("Please install MetaMask!");
+        return;
+      }
+
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * API to deploy the Wallet from factory 
+   */
+  const deployNewWallet = async () => {
+    let factoryAddress = config.factoryAddress;
+
+    if (!window.ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      factoryAddress,
+      Factory.abi,
+      signer
+    );
+
+    const deployed = await contract.createTLSCW();
+    console.log(deployed);
+  };
+
+  const isWalletDeployed = async() =>{
+
+    let factoryAddress = config.factoryAddress;
+
+    if (!window.ethereum) {
+      alert("Please install MetaMask!");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      factoryAddress,
+      Factory.abi,
+      signer
+    );
+    setFactoryContract(contract);
+
+    const walletAddress =  contract.getWallet();
+    if(walletAddress !== 0){ 
+      return true;
+    }
+    else return false;
+  }
+
+  const handleEthDeposit =  async(amount) =>{
+    if(isWalletDeployed()){
+      const walletAddress = await factoryContract.getWallet();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      const tx = await signer.sendTransaction({
+        to: walletAddress,
+        value : ethers.utils.parseEther(amount)
+      });
+
+      console.log("tx", tx);
+
+    }
+    else{
+
+    }
+  }
+
+  const handleTokenDeposit =  async() =>{
+
+  }
+  useEffect(() => {
+    connectWallet();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form onSubmit={handleEthDeposit}>
+        <h1>
+          Deposit ETH
+        </h1>
+        <input
+          type ="text"
+          name = "ethers"
+          placeholder = "Amount in ETh"
+        />
+      </form>
   );
 }
-
-export default App;
